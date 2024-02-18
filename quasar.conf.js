0ae852021316d27e18a49e2b34c3d9d6cc9c0034
zppp <zppp>
2024-02-18 23:24:40 +0800

    aa23467

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,8 +42,8 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      publicPath: './',
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      publicPath: '/',
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
 
